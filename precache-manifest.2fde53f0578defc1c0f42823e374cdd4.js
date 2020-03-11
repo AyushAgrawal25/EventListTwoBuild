607236a54f824ff7b1329be5f40d4ff1788f388b
Ayush Agrawal <agrawal.ayush2500@gmail.com>
2020-03-11 09:54:26 +0530

    Adding Build

---
+++ precache-manifest.2fde53f0578defc1c0f42823e374cdd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4c23473eb26a046b6df6a4c7508250",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7fd03a7d0fafc54c77b",
+    "url": "/static/css/main.8120f39b.chunk.css"
+  },
+  {
+    "revision": "b356f9db5ee643b221b2",
+    "url": "/static/js/2.b4a3779e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.b4a3779e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7fd03a7d0fafc54c77b",
+    "url": "/static/js/main.1abd08bf.chunk.js"
+  },
+  {
+    "revision": "f4015469b9d745a0e1a7",
+    "url": "/static/js/runtime-main.79a93ecc.js"
+  },
+  {
+    "revision": "7df14f25f541edd63e8fcaa1d1752b33",
+    "url": "/static/media/E-Summit.7df14f25.png"
+  }
+]);
